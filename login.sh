36600b03dae6fe2e64996e9e4fbd918145e8fb12
devanshi <kashapaka.devanshi@gmail.com>
2024-09-13 12:36:11 +0530

    login.sh commit

---
+++ login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+echo "Please enter your username: " 
+
+read -s USERNAME
+
+echo "Please enter your password: "
+
+read -s PASSWORD
+
+
+echo "The enter username is: $USERNAME and password is: $PASSWORD"
