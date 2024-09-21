34e46fe647a6841916bd0263cd533e6ffa990e3b
devanshi <kashapaka.devanshi@gmail.com>
2024-09-21 09:42:01 +0530

    greetings.sh

--- greetings.sh
+++ greetings.sh
@@ -16,6 +16,8 @@ do
     case $opt in
         n)NAME="OPTARG";;
         g)GREETING="OPTARG";;
+       \?) echo "Invalid Options: "$OPTARG" >&2; USAGE; exit;;
+        :) USAGE; exit;;
         h|*) USAGE; exit;;
     esac
 
