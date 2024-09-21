6e12090e98dd0bb074d1b1b6ecbf80d8e2dac8a7
devanshi <kashapaka.devanshi@gmail.com>
2024-09-21 13:28:25 +0530

    rs_infra_setup

--- rs_instances.sh
+++ rs_instances.sh
@@ -3,6 +3,8 @@
 AMI_ID="ami-0b4f379183e5706b9"
 SG_ID="sg-0efaa853ee917a890"
 INSTANCES=("WEB" "CATALOGUE" "CART" "PAYMENT" "SHIPPING" "REDIS" "MYSQL" "MONGODB" "RABBITMQ" "USER" "DISPATCH")
+HOSTED_ZONE_ID=Z04039613RABGV7GUAUIJ
+DOMAIN_NAME=roboriya.shop
 
 for i in "${INSTANCES[@]}"
 do 
@@ -20,7 +22,23 @@ IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE
 # --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text
 
 echo "$i:$IP_ADDRESS"
-
-
-
 done
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id "$HOSTED_ZONE_ID" \
+  --change-batch "
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "$i.$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+      }
+    }]
+  }
+  "
