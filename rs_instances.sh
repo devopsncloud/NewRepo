c78698ae7a66f1eb43f0419b213e43c70cd691a2
devanshi <kashapaka.devanshi@gmail.com>
2024-09-21 12:49:58 +0530

    rs_infra_setup

--- rs_instances.sh
+++ rs_instances.sh
@@ -15,9 +15,10 @@ else
 fi
 
 
-aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
 
 # --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text
+
 
 
 done
