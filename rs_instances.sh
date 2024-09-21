a81eed929ef20c9e3d35332fdd76353491b9e471
devanshi <kashapaka.devanshi@gmail.com>
2024-09-21 12:08:00 +0530

    rs_infra_setup

--- rs_instances.sh
+++ rs_instances.sh
@@ -15,7 +15,7 @@ else
 fi
 
 
-aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$}]"
+aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
 
 done
